e0c926c222ff58a19ae87fd15ba31b6ab320bb8e
rajeev <rajeevmaurya9794280914@gmail.com>
2020-10-04 23:23:35 +0530

    Updates

---
+++ precache-manifest.eb67a98e2ca0342f3c429e9ca610682d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf2d93a45b7c92887be8e4a708108f9",
+    "url": "/expense-manager/index.html"
+  },
+  {
+    "revision": "cd60186de0175bd2debc",
+    "url": "/expense-manager/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7915540dc0d568c73073",
+    "url": "/expense-manager/static/css/main.c147caf5.chunk.css"
+  },
+  {
+    "revision": "cd60186de0175bd2debc",
+    "url": "/expense-manager/static/js/2.7f37193b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-manager/static/js/2.7f37193b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7915540dc0d568c73073",
+    "url": "/expense-manager/static/js/main.7e4925b0.chunk.js"
+  },
+  {
+    "revision": "f47c406cda207e4fd088",
+    "url": "/expense-manager/static/js/runtime-main.f7c68bf5.js"
+  },
+  {
+    "revision": "ad87b8a3062bb72178502b6be2adc125",
+    "url": "/expense-manager/static/media/Expense_Life_Cycle.ad87b8a3.png"
+  },
+  {
+    "revision": "761eeba19f5bd184bd90ba802a7da76f",
+    "url": "/expense-manager/static/media/ex.761eeba1.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/expense-manager/static/media/logo.5d5d9eef.svg"
+  }
+]);
